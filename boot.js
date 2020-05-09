9b027a54b4d493e649876ea2e6e097c21881b03c
J.R. Morgan <liveaverage@users.noreply.github.com>
2020-05-08 20:43:13 -0400

    Update boot.js

--- boot.js
+++ boot.js
@@ -30,25 +30,31 @@ async function handleRequest(request) {
   var gw = getParameterByName('gw')
   var nm = getParameterByName('netmask')
   var hn = getParameterByName('hostname')
+  var ns = getParameterByName('ns')
 
-  var ka_ip = `ip='${ip}::${gw}:${nm}:${hn}:ens192:none`
-  var ka    = 'kernel \${coreos-url}/rhcos-\${release}.\${zstream}-${arch}-installer-kernel-\${arch} \${console} \${first-boot} \${auto-login} \${oem} ' + (ip ? ka_ip : '')
-  var init  = 'initrd \${coreos-url}/rhcos-\${zstream}-\${arch}-installer-initramfs.\${arch}.img'
-  var boot = `
-  #!ipxe
-  set release 4.3
-  set zstream 8
-  set arch x86_64
-  set coreos-url https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/\${release}/\${zstream}
-  set console console=ttyS1,115200n8
-  set first-boot coreos.first_boot=1
-  set auto-login coreos.autologin
-  set oem coreos.oem.id=packet
-  
-  ${ka}
-  ${init}
-  boot
-  `
+  var ka_ip = `ip=${ip}::${gw}:${nm}:${hn}:ens192:none nameserver=` + (ns ? ns : '1.1.1.1')
+  var ka    = 'kernel \${coreos-url}/rhcos-\${release}.\${zstream}-${arch}-installer-kernel-\${arch} \${console} \${first-boot} \${auto-login} \${oem} ' + (ip ? ka_ip : 'ip=dhcp')
+  var init  = '\${coreos-url}/rhcos-\${release}.\${zstream}-\${arch}-installer-initramfs.\${arch}.img'
+  var cs    = `rd.neednet=1 initrd=${init} coreos.inst=yes coreos.inst.install_dev=sda coreos.inst.image_url=\${coreos-img}`
+  var ign   = `coreos.inst.ignition_url=http://169.254.169.254/metadata/v1/user-data`
+
+  var boot = 
+`#!ipxe
+
+set release 4.3
+set zstream 8
+set arch x86_64
+set coreos-url https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/\${release}/\${release}.\${zstream}
+set coreos-img \${coreos-url}/rhcos-\${release}.\${zstream}-\${arch}-metal.\${arch}.raw.gz
+set console console=ttyS1,115200n8
+set first-boot coreos.first_boot=1
+set auto-login coreos.autologin
+set oem coreos.oem.id=packet
+ 
+${ka} ${cs} ${ign}
+initrd ${init}
+boot
+`
 
   return new Response(boot, {
     headers: { 'content-type': 'text/plain' },
